fc4c656c06d8d3d4b1d688680d8b3eecc8024842
JeromeSoh <124503129+JeromeSoh@users.noreply.github.com>
2023-02-10 15:21:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ if choice == "2. Classification":
     # Create an empty list to store the completions
     completions = []
     df1.columns = ['Prompt','Completion'] 
-    df1['Completion']=df1['Completion'].astype(str)
+    df1['Prompt']=df1['Prompt'].astype(str)
     
     # Loop through each row in the DataFrame
     for index, row in df1.iterrows():
